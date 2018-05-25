9b39beb712ad10e0cb507e1f03599c90bc525f4c
lalit bansal <lalit.bansal457@gmail.com>
2018-05-25 15:13:46 +0530

    Added comments

--- controller.js
+++ controller.js
@@ -5,29 +5,39 @@ app.controller('iScrollCtrl', function($scope, $window, $document, $timeout){
 	$scope.arr = [];
 	$scope.limitScroll = 50;
 	$scope.currentPage = 0;
-
+	
+	// 
 	for(i=1; i<=$scope.max; i++) {
 		$scope.arr.push(i);
 	}
+	
+	// load  data 
 	$scope.loadMore = function(){
 		$scope.currentPage += 1;
 	}
+	
+	// remove data
 	$scope.removeMore = function(){
 		if($scope.currentPage > 0) {
 			$scope.currentPage -= 1;
 		}
 	}
+	
+	// calculate document height
 	$timeout(function(){
 		$scope.docHeight = $document.height();
 	});
 });
+
+
 
 app.directive('scroll', function($window, $document){
 	return {
 		restrict: 'A',
 		link: function(scope, element, attr) {
 			scope.lastScrollTop = 0;
-
+			
+			// bind scroll event
 			angular.element($window).bind('scroll', function(){
 				scope.st = window.pageYOffset;
 
